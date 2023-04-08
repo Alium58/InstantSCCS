0d60916e3df92e765fa5ceb6128d588b2f5d8d25
Alium58 <bennelson3927@gmail.com>
2023-04-07 20:49:05 -0500

    decrease target myst to 165

--- src/tasks/leveling.ts
+++ src/tasks/leveling.ts
@@ -82,7 +82,7 @@ import {
   rufusTarget,
 } from "libram/dist/resources/2023/ClosedCircuitPayphone";
 
-const targetBaseMyst = 190;
+const targetBaseMyst = 165;
 const baseBoozes = $items`bottle of rum, boxed wine, bottle of gin, bottle of vodka, bottle of tequila, bottle of whiskey`;
 const freeFightMonsters: Monster[] = $monsters`Witchess Bishop, Witchess King, Witchess Witch, sausage goblin, Eldritch Tentacle`;
 const craftedCBBFoods: Item[] = $items`honey bun of Boris, roasted vegetable of Jarlsberg, Pete's rich ricotta, plain calzone`;
